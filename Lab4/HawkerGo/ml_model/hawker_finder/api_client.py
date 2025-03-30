e387e9b77ad774c8936e9513f1a63585b9f8b9eb
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2025-03-31 04:48:43 +0800

    feat: Add Client for Google Places API
    Client currently allows:
    - Search for a location based on text query
    - Search for nearby locations given coordinates
    - Search for location details based on its id

---
+++ Lab4/HawkerGo/ml_model/hawker_finder/api_client.py
@@ -0,0 +1,179 @@
+""" Contains the GooglePlacesAPIClient class responsible for making requests to the Google Places API. """
+
+import requests
+
+class GooglePlacesAPIClient:
+    # Only include the following fields in the response by default.
+    # This is to reduce the amount of data returned by the API
+    # which can help improve performance and reduce costs
+    _TEXT_SEARCH_MASKS_DEFAULT = ",".join([
+        "places.id",
+        "places.formattedAddress",
+        "places.displayName",
+        "places.location"
+    ])
+    
+    _NEARBY_SEARCH_MASKS_DEFAULT = ",".join([
+        "places.id",
+        "places.types",
+        "places.formattedAddress",
+        "places.displayName",
+        "places.location"
+    ])
+    
+    def __init__(self, api_key):
+        self.api_key = api_key
+        
+    def requestTextSearch(self,
+                          query: str,
+                          count: int = 20,
+                          search_mask: str = _TEXT_SEARCH_MASKS_DEFAULT) -> list[dict]:
+        """Makes a request to the Google Places **Text Search (New)** API.
+
+        Args:
+            query (str): The query to search for.
+            count (int): The max number of places to return. Note the number of API calls
+                made is equal to the number of places returned divided by 20, rounded up.
+            search_mask (str): The fields to include in the response.
+                Recommended to use the default value for performance and cost reasons.
+                Refer to the [Google Places API documentation](https://developers.google.com/maps/documentation/places/web-service/text-search#fieldmask) for more info.
+
+        Returns:
+            dict: The list of places and their details. 
+                Only includes the fields specified in the search_mask.
+        """
+        
+        if count > 20:
+            # If the count is greater than 20, we need to include the nextPageToken
+            # field in the response to get the next page of results
+            # This is because the API only returns max 20 results per page
+            if "nextPageToken" not in search_mask:
+                search_mask += f",nextPageToken"
+        
+        uri = "https://places.googleapis.com/v1/places:searchText"
+        headers = {
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": self.api_key,
+            # Only include the following fields in the response
+            "X-Goog-FieldMask": search_mask
+        }
+        body = { "textQuery": query }
+        
+        retrieved_places = []
+        retrieved_count = 0
+        
+        # Loop until we have retrieved the desired number of places
+        while retrieved_count < count:
+            # Set the pageSize field in the body to
+            #   get the exact number of results
+            #   we want in the response.
+            #   The API only allows max 20 results per page
+            body["pageSize"] = min(count - retrieved_count, 20)
+            
+            response = requests.post(uri, headers=headers, json=body)
+        
+            if response.status_code != 200:
+                raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
+            
+            # Get the JSON response
+            response_json = response.json()
+            
+            # If there are no results, break out of the loop
+            if "places" not in response_json:
+                break
+            
+            # Update the list and count of retrieved places
+            retrieved_places.extend(response_json["places"])
+            retrieved_count += len(response_json["places"])
+            
+            # If there are no more results,
+            #   nextPageToken will not be in the response
+            #   so we can break out of the loop
+            if "nextPageToken" not in response_json:
+                break
+            
+            # Get the next page of results by setting the pageToken field in the body
+            body["pageToken"] = response_json["nextPageToken"]
+        
+        return retrieved_places
+
+    def requestNearbySearch(self,
+                            latitude: float,
+                            longitude: float,
+                            radius: float,
+                            types: list[str],
+                            search_mask: str = _NEARBY_SEARCH_MASKS_DEFAULT) -> dict:
+        """Makes a request to the Google Places **Nearby Search (New)** API.
+
+        Args:
+            latitude (float): The latitude of the location to search around.
+            longitude (float): The longitude of the location to search around.
+            radius (float): The radius to search within.
+            types (list[str]): The types of places to search for. For a list of
+                supported types, refer to the [Google Places API documentation](https://developers.google.com/maps/documentation/places/web-service/place-types)
+            search_mask (str): The fields to include in the response.
+                Recommended to use the default value for performance and cost reasons.
+                Refer to the [Google Places API documentation](https://developers.google.com/maps/documentation/places/web-service/nearby-search#fieldmask) for more info.
+
+        Returns:
+            dict: The JSON response from the API. Returns top 20 results by distance.
+                Only includes the fields specified in the search_mask.
+        """
+        
+        uri = "https://places.googleapis.com/v1/places:searchNearby"
+        headers = {
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": self.api_key,
+            # Only include the following fields in the response
+            "X-Goog-FieldMask": search_mask
+        }
+        body = {
+            "locationRestriction": {
+                "circle": {
+                    "center": {
+                        "latitude": latitude,
+                        "longitude": longitude
+                    },
+                    "radius": radius
+                }
+            },
+            "includedTypes": types,
+            "rankPreference": "DISTANCE"
+        }
+        
+        response = requests.post(uri, headers=headers, json=body)
+        
+        if response.status_code != 200:
+            raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
+        
+        return response.json()["places"] if "places" in response.json() else []
+    
+    def requestPlaceDetails(self,
+                            place_id: str,
+                            search_mask: str = "places.id") -> dict:
+        """Makes a request to the Google Places **Place Details (New)** API.
+
+        Args:
+            place_id (str): The unique identifier of the place.
+            search_mask (str): The fields to include in the response.
+                Default value set to avoid unnecessary data transfer when calling this function.
+                Refer to the [Google Places API documentation](https://developers.google.com/maps/documentation/places/web-service/details#fieldmask) for more info.
+
+        Returns:
+            dict: The place details. Only includes the fields specified in the search_mask.
+        """
+        
+        uri = f"https://places.googleapis.com/v1/places/{place_id}"
+        headers = {
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": self.api_key,
+            # Only include the following fields in the response
+            "X-Goog-FieldMask": search_mask
+        }
+        
+        response = requests.get(uri, headers=headers)
+        
+        if response.status_code != 200:
+            raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
+        
+        return response.json()
