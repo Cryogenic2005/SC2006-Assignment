f9c11a88b7405bc6fc889ccc88ad51fcb2097af5
Razan1412 <167756771+Razan1412@users.noreply.github.com>
2025-04-09 01:05:36 +0800

    Commented out config dependency, TEMP FIX

--- Lab4/HawkerGo/server/src/services/mlService.js
+++ Lab4/HawkerGo/server/src/services/mlService.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const config = require('../config');
+//const config = require('../config');
 
 const ML_API_BASE_URL = process.env.ML_API_URL || 'http://localhost:5000';
 
