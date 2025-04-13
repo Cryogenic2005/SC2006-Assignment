dee6b5d79bae92ab93d11e7c85a4112833788923
Razan1412 <razan1412siraj@gmail.com>
2025-04-14 00:55:02 +0800

    Fixed logs for demo

--- Lab4/HawkerGo/client/app/index.js
+++ Lab4/HawkerGo/client/app/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store'; 
 import MainNavigation from './navigation/MainNavigation';
+import { LogBox } from 'react-native';
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
 
 export default function AppEntry() {
   return (
